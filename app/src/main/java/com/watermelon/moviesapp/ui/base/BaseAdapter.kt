5b31f6c087c6f812b48506a733593b3e13bbbb72
Al-Taie <agprosup@gmail.com>
2021-10-25 17:41:45 +0300

    feat: create base adapter

---
+++ app/src/main/java/com/watermelon/moviesapp/ui/base/BaseAdapter.kt
@@ -0,0 +1,43 @@
+package com.watermelon.moviesapp.ui.base
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.recyclerview.widget.RecyclerView
+
+interface BaseInteractionListener
+
+abstract class BaseAdapter<T>(
+    private var _items: List<T>,
+    private val _listener: BaseInteractionListener
+) : RecyclerView.Adapter<BaseAdapter.BaseViewHolder>() {
+    val items get() = _items
+    override fun getItemCount() = _items.size
+    fun setItems(newItems: List<T>) = run { _items = newItems }
+
+    abstract val layoutID: Int
+
+    class ItemViewHolder(val binding: ViewDataBinding) : BaseViewHolder(binding)
+    abstract class BaseViewHolder(binding: ViewDataBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        return ItemViewHolder(DataBindingUtil.inflate(inflater, layoutID, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: BaseViewHolder, position: Int) {
+        val currentItem = _items[position]
+        when (holder) {
+            is ItemViewHolder -> {
+                holder.binding.apply {
+//                    setVariable(BR.item, currentItem)
+//                    setVariable(BR.listener, _listener)
+                }
+            }
+        }
+    }
+}
+
+
+
