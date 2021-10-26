cf7f2f102385595e285025a70259b65235d9b087
Sahar Qais <sahar.qais.k@gmail.com>
2021-10-26 21:03:47 +0300

    feat: create details fragment

---
+++ app/src/main/java/com/watermelon/moviesapp/ui/details/DetailsFragment.kt
@@ -0,0 +1,19 @@
+package com.watermelon.moviesapp.ui.details
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.watermelon.moviesapp.ui.base.BaseFragment
+import com.watermelon.moviesapp.viewModels.DetailsViewModel
+import watermelon.moviesapp.databinding.DetailsFragmentBinding
+
+class DetailsFragment : BaseFragment<DetailsFragmentBinding>(){
+    override fun setup() {
+    }
+
+    override val viewModelID: Int
+        get() = TODO("Not yet implemented")
+    override val viewModel: DetailsViewModel by viewModels()
+    override val inflate: (LayoutInflater, ViewGroup?, attachToRoot: Boolean) -> DetailsFragmentBinding
+        get() = DetailsFragmentBinding::inflate
+}
