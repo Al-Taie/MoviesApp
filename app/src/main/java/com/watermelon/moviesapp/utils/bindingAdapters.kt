d93cad3b4c6a58a955bffa7a8895a142d8139c05
Ammar Thaar <amar.at909@gmail.com>
2021-10-31 20:58:39 +0300

    feat: add some binding adapters

--- app/src/main/java/com/watermelon/moviesapp/utils/bindingAdapters.kt
+++ app/src/main/java/com/watermelon/moviesapp/utils/bindingAdapters.kt
@@ -1,18 +1,56 @@
 package com.watermelon.moviesapp.utils
 
+import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.google.android.material.imageview.ShapeableImageView
+import com.watermelon.moviesapp.model.State
 import com.watermelon.moviesapp.ui.base.BaseAdapter
+
+
+@BindingAdapter(value = ["app:showWhenLoading"])
+fun <T> showWhenLoading(view: View, state: State<T>?) {
+    when (state) {
+        is State.Loading -> view.visibility = VISIBLE
+        else -> view.visibility = GONE
+    }
+
+}
+
+@BindingAdapter(value = ["app:showWhenError"])
+fun <T> showWhenError(view: View, state: State<T>?) {
+    when (state) {
+        is State.Error ->
+            view.visibility = VISIBLE
+
+        else ->
+            view.visibility = GONE
+
+    }
+}
+
+@BindingAdapter(value = ["app:showWhenSuccess"])
+fun <T> showWhenSuccess(view: View, state: State<T>?) {
+    when (state) {
+        is State.Success ->
+            view.visibility = VISIBLE
+
+        else ->
+            view.visibility = GONE
+
+    }
+}
 
 
 @BindingAdapter(value = ["app:items"])
 fun <T> setRecyclerItems(view: RecyclerView, items: List<T>?) {
-    if (items!=null){
+    if (items != null) {
 
         (view.adapter as BaseAdapter<T>?)?.setItems(items)
-    }else
+    } else
         (view.adapter as BaseAdapter<T>?)?.setItems(emptyList())
 
 }
