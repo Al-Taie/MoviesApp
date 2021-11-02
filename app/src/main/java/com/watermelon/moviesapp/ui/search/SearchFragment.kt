1abdc363d2a862d0396ea1ce7c1f2c8785914d25
Al-Taie <agprosup@gmail.com>
2021-11-02 23:21:31 +0300

    fix: navigate event

--- app/src/main/java/com/watermelon/moviesapp/ui/search/SearchFragment.kt
+++ app/src/main/java/com/watermelon/moviesapp/ui/search/SearchFragment.kt
@@ -1,37 +1,29 @@
 package com.watermelon.moviesapp.ui.search
 
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
-import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import com.watermelon.moviesapp.ui.base.BaseFragment
-import com.watermelon.moviesapp.utils.EventObserver
-import com.watermelon.moviesapp.viewModels.SearchViewModel
+import com.watermelon.moviesapp.utils.Event
 import watermelon.moviesapp.databinding.FragmentSearchBinding
 
 class SearchFragment : BaseFragment<FragmentSearchBinding>() {
-
-
     override val viewModel: SearchViewModel by activityViewModels()
     override val inflate: (LayoutInflater, ViewGroup?, attachToRoot: Boolean) -> FragmentSearchBinding
         get() = FragmentSearchBinding::inflate
 
     override fun setup() {
-        binding.searchRecyclerView.adapter =SearchAdapter(mutableListOf(),viewModel)
-        Log.i("search", viewModel.movieTitle.toString())
-
-        viewModel.movieTitle.observe(this, {
-            Log.i("observe", it.toString())
-        })
-
-        viewModel.openTaskEvent.observe(this, EventObserver {
-//            val action = SearchFragmentDirections.actionSearchFragmentToDetailsFragment(R.id.action_searchFragment_to_detailsFragment)
-
-            val action = SearchFragmentDirections.actionSearchFragmentToDetailsFragment(movieId = )
-            this.view?.let { it1 -> Navigation.findNavController(it1).navigate(action) }
-        })
+        binding.viewModel = viewModel
+        binding.searchRecyclerView.adapter =SearchAdapter(mutableListOf(), viewModel)
+        viewModel.navigateToDetails.observe(this, ::onNavigate)
     }
 
 
+    private fun onNavigate(event: Event<Int>) {
+        event.getContentIfNotHandled()?.let {  movieId ->
+            val action = SearchFragmentDirections.actionSearchFragmentToDetailsFragment(movieId)
+            findNavController().navigate(action)
+        }
+    }
 }
--- app/src/main/java/com/watermelon/moviesapp/ui/search/SearchViewModel.kt
+++ app/src/main/java/com/watermelon/moviesapp/ui/search/SearchViewModel.kt
@@ -1,34 +1,25 @@
 package com.watermelon.moviesapp.ui.search
 
-import android.util.Log
-import android.view.View
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.navigation.Navigation
 import com.watermelon.moviesapp.model.State
 import com.watermelon.moviesapp.model.repository.MovieRepository
+import com.watermelon.moviesapp.model.response.movie.Movie
 import com.watermelon.moviesapp.model.response.movie.MovieResponse
 import com.watermelon.moviesapp.ui.home.MovieInteractionListener
-import com.watermelon.moviesapp.ui.search.SearchFragment
-import com.watermelon.moviesapp.ui.search.SearchFragmentDirections
 import com.watermelon.moviesapp.utils.Event
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
-import watermelon.moviesapp.R
 
-class SearchViewModel : ViewModel(),MovieInteractionListener {
-
-
+class SearchViewModel : ViewModel(), MovieInteractionListener{
     val movieTitle = MutableLiveData<String?>()
-
     var movieSearchResult = MutableLiveData<State<MovieResponse?>>()
     var movieDetails = MutableLiveData<State<Movie?>>()
 
-
-    private val _openTaskEvent = MutableLiveData<Event<String>>()
-    val openTaskEvent: LiveData<Event<String>> = _openTaskEvent
+    private val _navigateToDetails = MutableLiveData<Event<Int>>()
+    val navigateToDetails: LiveData<Event<Int>> = _navigateToDetails
 
     fun searchForMovie() {
 
@@ -39,11 +30,12 @@ class SearchViewModel : ViewModel(),MovieInteractionListener {
         }
     }
 
-    override fun onItemClicked(movie: Movie) {
+    override fun onItemClicked(movieId: Int) {
+        _navigateToDetails.postValue(Event(movieId))
+
         viewModelScope.launch {
-            MovieRepository.getMovieDetails(movie.id!!).collect {
+            MovieRepository.getMovieDetails(movieId).collect {
                 movieDetails.postValue(it)
-                Log.i("details",it.toString())
             }
         }
     }
