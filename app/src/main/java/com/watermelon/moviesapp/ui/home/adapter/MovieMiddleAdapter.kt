e2c5d56723b61def951a1d40024d0fa91d567eb0
Al-Taie <agprosup@gmail.com>
2021-11-08 03:49:19 +0300

    feat: implement nested adapter with ui

--- app/src/main/java/com/watermelon/moviesapp/ui/home/adapter/MovieMiddleAdapter.kt
+++ app/src/main/java/com/watermelon/moviesapp/ui/home/adapter/MovieMiddleAdapter.kt
@@ -1,12 +1,13 @@
 package com.watermelon.moviesapp.ui.home.adapter
 
 import com.watermelon.moviesapp.model.response.movie.Movie
+import com.watermelon.moviesapp.model.response.movies.Movies
 import com.watermelon.moviesapp.ui.base.BaseAdapter
 import com.watermelon.moviesapp.ui.home.MovieInteractionListener
 import watermelon.moviesapp.R
 
-class MovieMiddleAdapter(list: List<Movie>, listener: MovieInteractionListener) :
-        BaseAdapter<Movie>(list, listener) {
+class MovieMiddleAdapter(list: List<Movies>, listener: MovieInteractionListener) :
+        BaseAdapter<Movies>(list, listener) {
         override var layoutID = R.layout.item_home_middle_recycler
 
 }
--- app/src/main/java/com/watermelon/moviesapp/ui/home/adapter/MovieTopAdapter.kt
+++ app/src/main/java/com/watermelon/moviesapp/ui/home/adapter/MovieTopAdapter.kt
@@ -1,18 +1,15 @@
 package com.watermelon.moviesapp.ui.home.adapter
 
+import com.watermelon.moviesapp.main.MainViewModel
 import com.watermelon.moviesapp.model.response.movie.Movie
+import com.watermelon.moviesapp.model.response.movies.Movies
 import com.watermelon.moviesapp.ui.base.BaseAdapter
 import com.watermelon.moviesapp.ui.home.HomeViewModel
 import com.watermelon.moviesapp.ui.home.MovieInteractionListener
 import watermelon.moviesapp.BR
 import watermelon.moviesapp.R
 
-class MovieTopAdapter(list: List<Movie>, listener: MovieInteractionListener, val viewModel: HomeViewModel) :
-        BaseAdapter<Movie>(list, listener) {
+class MovieTopAdapter(list: List<Movies>, listener: MovieInteractionListener) :
+        BaseAdapter<Movies>(list, listener) {
         override var layoutID = R.layout.item_home_top_recycler
-
-        override fun onBindViewHolder(holder: BaseViewHolder, position: Int) {
-                super.onBindViewHolder(holder, position)
-                (holder as ItemViewHolder).binding.setVariable(BR.viewModel, viewModel)
-        }
 }
