5b5322877969956c32b1b40c95b47a69e53b0524
Al-Taie <agprosup@gmail.com>
2021-11-10 15:51:25 +0300

    fix: tv ui & recycler

--- app/src/main/java/com/watermelon/moviesapp/ui/tv/TvFragment.kt
+++ app/src/main/java/com/watermelon/moviesapp/ui/tv/TvFragment.kt
@@ -1,51 +1,24 @@
 package com.watermelon.moviesapp.ui.tv
 
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
-import com.watermelon.moviesapp.model.State
-import com.watermelon.moviesapp.model.response.channel.Channel
-import com.watermelon.moviesapp.model.response.channel.ChannelResponse
-import com.watermelon.moviesapp.model.response.trending.all.TrendingAll
-import com.watermelon.moviesapp.model.response.trending.movie.TrendingMovie
-import com.watermelon.moviesapp.model.response.trending.person.TrendingPerson
-import com.watermelon.moviesapp.model.response.trending.tv.TrendingTV
-import com.watermelon.moviesapp.model.response.tv.Season
-import com.watermelon.moviesapp.model.response.tv.TvResponse
+import com.mig35.carousellayoutmanager.CarouselLayoutManager
+import com.mig35.carousellayoutmanager.CarouselZoomPostLayoutListener
 import com.watermelon.moviesapp.ui.base.BaseFragment
-import com.watermelon.moviesapp.ui.trending.adapter.TrendingNestedAdapter
-import com.watermelon.moviesapp.ui.tv.adapter.TvNestedAdapter
-import com.watermelon.moviesapp.ui.tv.adapter.TvViewType
+import com.watermelon.moviesapp.ui.tv.adapter.TvChannelAdapter
 import watermelon.moviesapp.databinding.FragmentTvBinding
 
 class TvFragment : BaseFragment<FragmentTvBinding>() {
 
     override fun setup() {
-        val items = mutableListOf(
-            emptyList<Season>(),
-            emptyList<Channel>()
-        )
-        val adapter = TvNestedAdapter(items, viewModel)
-        viewModel.latestTvShow.observe(this, { state ->
-            if (state is State.Success) {
-                state.data?.seasons?.let { data ->
-                    Log.v("TESTING", data.toString())
-                    adapter.setItemsAt(data, TvViewType.TV_SEASON.index)
-                    binding.adapter = adapter
-                }
+        binding.tvRecyclerView.apply {
+            adapter = TvChannelAdapter(emptyList(), viewModel)
+            layoutManager = CarouselLayoutManager(CarouselLayoutManager.VERTICAL).apply {
+                setPostLayoutListener(CarouselZoomPostLayoutListener())
+                setHasFixedSize(true)
             }
-        })
-
-        viewModel.tvShowChannel.observe(this, { state ->
-            if (state is State.Success) {
-                state.data?.results?.let { data ->
-                    Log.v("TESTING", data.toString())
-                    adapter.setItemsAt(data, TvViewType.TV_CHANNEL.index)
-                    binding.adapter = adapter
-                }
-            }
-        })
+        }
     }
 
     override val viewModel: TvViewModel by activityViewModels()
--- app/src/main/java/com/watermelon/moviesapp/ui/tv/TvViewModel.kt
+++ app/src/main/java/com/watermelon/moviesapp/ui/tv/TvViewModel.kt
@@ -5,14 +5,11 @@ import androidx.lifecycle.asLiveData
 import com.watermelon.moviesapp.model.repository.MovieRepository
 
 class TvViewModel:ViewModel(),TvInteractionListener {
-
-
     val latestTvShow = MovieRepository.getLatestTvShow().asLiveData()
 
     val tvShowChannel = MovieRepository.getTvShowChannel().asLiveData()
 
     val getTvShowProvidersChannel = MovieRepository.getTvShowProvidersChannel().asLiveData()
-
 
     override fun onItemLoad(id: Int) {}
 
