f8429ed479371391cc679571239e4a897acb1ab1
Al-Taie <agprosup@gmail.com>
2021-11-14 10:56:52 +0300

    feat: add navigate from tv to details

--- app/src/main/java/com/watermelon/moviesapp/ui/tv/TvFragment.kt
+++ app/src/main/java/com/watermelon/moviesapp/ui/tv/TvFragment.kt
@@ -1,11 +1,11 @@
 package com.watermelon.moviesapp.ui.tv
 
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
-import com.watermelon.moviesapp.model.State
+import androidx.navigation.fragment.findNavController
 import com.watermelon.moviesapp.ui.base.BaseFragment
+import com.watermelon.moviesapp.utils.Event
 import watermelon.moviesapp.databinding.FragmentTvBinding
 
 class TvFragment : BaseFragment<FragmentTvBinding>() {
@@ -14,10 +14,18 @@ class TvFragment : BaseFragment<FragmentTvBinding>() {
         binding.tvRecyclerView.adapter = TVAdapter(emptyList(), viewModel)
         viewModel.run {
             onTheAir.observe(this@TvFragment, { stream.postValue(it) })
+            navigateToDetails.observe(this@TvFragment, ::onNavigate)
         }
     }
 
     override val viewModel: TvViewModel by activityViewModels()
     override val inflate: (LayoutInflater, ViewGroup?, attachToRoot: Boolean) -> FragmentTvBinding
         get() = FragmentTvBinding::inflate
+
+    private fun onNavigate(event: Event<Int>) {
+        event.getContentIfNotHandled()?.let { movieID ->
+            val action = TvFragmentDirections.actionTvFragmentToDetailsFragment(movieID)
+            findNavController().navigate(action)
+        }
+    }
 }
--- app/src/main/java/com/watermelon/moviesapp/ui/tv/TvViewModel.kt
+++ app/src/main/java/com/watermelon/moviesapp/ui/tv/TvViewModel.kt
@@ -1,11 +1,13 @@
 package com.watermelon.moviesapp.ui.tv
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import com.watermelon.moviesapp.model.State
 import com.watermelon.moviesapp.model.repository.MovieRepository
 import com.watermelon.moviesapp.model.response.tv.TVResponse
+import com.watermelon.moviesapp.utils.Event
 
 class TvViewModel : ViewModel(), TvInteractionListener {
     val topRated = MovieRepository.getTvTopRated().asLiveData()
@@ -15,7 +17,11 @@ class TvViewModel : ViewModel(), TvInteractionListener {
 
     val stream: MutableLiveData<State<TVResponse?>> = MutableLiveData()
 
+    private val _navigateToDetails = MutableLiveData<Event<Int>>()
+    val navigateToDetails: LiveData<Event<Int>> = _navigateToDetails
+
     override fun onItemLoad(id: Int) {}
 
-    override fun onItemClicked(id: Int) {}
+    override fun onItemClicked(id: Int) = _navigateToDetails.postValue(Event(id))
+
 }
