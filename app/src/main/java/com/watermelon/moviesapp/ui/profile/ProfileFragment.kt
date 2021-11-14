cb55f09ed9a80f18c68ccf8f8dfdc537ecf23ead
Sahar Qais <sahar.qais.k@gmail.com>
2021-11-14 03:13:01 +0300

    feat: about person API result

--- app/src/main/java/com/watermelon/moviesapp/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/watermelon/moviesapp/ui/profile/ProfileFragment.kt
@@ -3,22 +3,29 @@ package com.watermelon.moviesapp.ui.profile
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.google.android.material.tabs.TabLayoutMediator
 import com.watermelon.moviesapp.ui.base.BaseFragment
+import com.watermelon.moviesapp.ui.details.DetailsFragmentDirections
 import com.watermelon.moviesapp.ui.profile.about.AboutFragment
 import com.watermelon.moviesapp.ui.profile.movies.MoviesActorFragment
 import com.watermelon.moviesapp.ui.profile.tv.TvShowsActorFragment
 import watermelon.moviesapp.R
 import watermelon.moviesapp.databinding.FragmentProfileBinding
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.viewModels
+
 
 class ProfileFragment : BaseFragment<FragmentProfileBinding>() {
     private val args: ProfileFragmentArgs by navArgs()
     val fragments = listOf(AboutFragment(),MoviesActorFragment(),TvShowsActorFragment())
-    val tabTitles = listOf(getString(R.string.about) , getString(R.string.movies),getString(R.string.tv_shows))
+    val tabTitles = listOf("About" , "Movies","Tv Shows")
 
 
-    override val viewModel: ProfileViewModel by activityViewModels()
+
+    override val viewModel: ProfileViewModel by viewModels()
     override val inflate: (LayoutInflater, ViewGroup?, attachToRoot: Boolean) -> FragmentProfileBinding
         get() = FragmentProfileBinding::inflate
 
@@ -26,7 +33,10 @@ class ProfileFragment : BaseFragment<FragmentProfileBinding>() {
         viewModel.onItemLoad(args.personId)
         initViewPager()
         initTabLayout()
+
+
     }
+
 
     private fun initTabLayout() {
         TabLayoutMediator(binding.tabLayout , binding.profileViewPager){ tab , position ->
--- app/src/main/java/com/watermelon/moviesapp/ui/profile/about/AboutViewModel.kt
+++ app/src/main/java/com/watermelon/moviesapp/ui/profile/about/AboutViewModel.kt
@@ -1,6 +1,27 @@
 package com.watermelon.moviesapp.ui.profile.about
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.watermelon.moviesapp.model.State
+import com.watermelon.moviesapp.model.repository.MovieRepository
+import com.watermelon.moviesapp.model.response.person.Person
+import com.watermelon.moviesapp.ui.home.MovieInteractionListener
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
-class AboutViewModel: ViewModel() {
+class AboutViewModel: ViewModel(),MovieInteractionListener{
+//    var person = MutableLiveData<State<Person?>>()
+
+    override fun onItemLoad(id: Int) {
+//        viewModelScope.launch {
+//            MovieRepository.getProfile(id).collect {
+//                person.postValue(it)
+//            }
+//        }
+    }
+
+    override fun onCastClicked(personId: Int) {
+        TODO("Not yet implemented")
+    }
 }
