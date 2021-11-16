26fe5b5d8d1b40b1f8125b479356f62e6ca5d554
Ammar Thaar <amar.at909@gmail.com>
2021-11-17 02:28:16 +0300

    fix:  add transition from popular to profile

--- app/src/main/java/com/watermelon/moviesapp/ui/popular/PopularFragment.kt
+++ app/src/main/java/com/watermelon/moviesapp/ui/popular/PopularFragment.kt
@@ -3,7 +3,9 @@ package com.watermelon.moviesapp.ui.popular
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import com.watermelon.moviesapp.ui.base.BaseFragment
+import com.watermelon.moviesapp.utils.Event
 import watermelon.moviesapp.databinding.FragmentPopularBinding
 
 class PopularFragment : BaseFragment<FragmentPopularBinding>() {
@@ -16,5 +18,14 @@ class PopularFragment : BaseFragment<FragmentPopularBinding>() {
     override fun setup() {
         binding.viewModel = viewModel
         binding.popularRecyclerDisplay.adapter = PopularAdapter(mutableListOf(), viewModel)
+        viewModel.navigateToProfile.observe(this, ::onNavigate)
+    }
+
+
+    private fun onNavigate(event: Event<Int>) {
+        event.getContentIfNotHandled()?.let { movieId ->
+            val action = PopularFragmentDirections.actionPopularFragmentToProfileFragment(movieId)
+            findNavController().navigate(action)
+        }
     }
 }
