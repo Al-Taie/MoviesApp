ae7a2d3bd384c5b0fd865f52babe110c15b1e318
Al-Taie <agprosup@gmail.com>
2021-11-16 22:41:14 +0300

    fix: add navigate from actor movies to movie details

--- app/src/main/java/com/watermelon/moviesapp/ui/profile/movies/MoviesActorFragment.kt
+++ app/src/main/java/com/watermelon/moviesapp/ui/profile/movies/MoviesActorFragment.kt
@@ -3,18 +3,26 @@ package com.watermelon.moviesapp.ui.profile.movies
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import com.watermelon.moviesapp.ui.base.BaseFragment
+import com.watermelon.moviesapp.ui.profile.ProfileFragmentDirections
 import com.watermelon.moviesapp.utils.Constant
+import com.watermelon.moviesapp.utils.EventObserver
 import watermelon.moviesapp.databinding.FragmentMoviesActorBinding
 
 class MoviesActorFragment : BaseFragment<FragmentMoviesActorBinding>() {
     override fun setup() {
         arguments?.getInt(Constant.ID)?.run { viewModel.onItemLoad(this) }
-
+        viewModel.navigateToDetails.observe(this, EventObserver { onNavigate(it) })
         binding.movieRecycler.adapter = CastMovieAdapter(emptyList(), viewModel)
     }
 
     override val viewModel: MoviesActorViewModel by activityViewModels()
     override val inflate: (LayoutInflater, ViewGroup?, attachToRoot: Boolean) -> FragmentMoviesActorBinding
         get() = FragmentMoviesActorBinding::inflate
+
+    private fun onNavigate(movieID: Int) {
+        val action = ProfileFragmentDirections.actionProfileFragmentToDetailsFragment(movieID)
+        findNavController().navigate(action)
+    }
 }
--- app/src/main/java/com/watermelon/moviesapp/ui/profile/movies/MoviesActorViewModel.kt
+++ app/src/main/java/com/watermelon/moviesapp/ui/profile/movies/MoviesActorViewModel.kt
@@ -1,5 +1,6 @@
 package com.watermelon.moviesapp.ui.profile.movies
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -7,21 +8,22 @@ import com.watermelon.moviesapp.model.State
 import com.watermelon.moviesapp.model.repository.MovieRepository
 import com.watermelon.moviesapp.model.response.movieCredits.MovieCredits
 import com.watermelon.moviesapp.ui.home.MovieInteractionListener
+import com.watermelon.moviesapp.utils.Event
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 class MoviesActorViewModel : ViewModel(),MovieInteractionListener {
     var movieCredits = MutableLiveData<State<MovieCredits?>>()
+    private val _navigateToDetails = MutableLiveData<Event<Int>>()
+    val navigateToDetails: LiveData<Event<Int>> = _navigateToDetails
+
+    override fun onItemClicked(id: Int) = _navigateToDetails.postValue(Event(id))
 
     override fun onItemLoad(id: Int) {
         viewModelScope.launch {
-
             MovieRepository.getMovieCredits(id).collect {
                 movieCredits.postValue(it)
             }
         }
-    }
-
-    override fun onCastClicked(personId: Int) {
     }
 }
