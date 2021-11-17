a545bb9ccb2a221dc5113ca42b9e8b88816946e5
Al-Taie <agprosup@gmail.com>
2021-11-18 01:56:35 +0300

    fix: status bar icons color

--- app/src/main/java/com/watermelon/moviesapp/ui/MainActivity.kt
+++ app/src/main/java/com/watermelon/moviesapp/ui/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.watermelon.moviesapp.ui
 
+import android.graphics.Color
+import android.os.Build
+import android.view.View
+import android.view.Window
 import androidx.activity.viewModels
+import androidx.annotation.RequiresApi
 import androidx.navigation.findNavController
 import com.watermelon.moviesapp.ui.base.BaseActivity
 import com.watermelon.moviesapp.utils.onNavDestinationSelected
 import watermelon.moviesapp.R
 import watermelon.moviesapp.databinding.ActivityMainBinding
+import androidx.core.view.WindowInsetsControllerCompat
+
+
+
 
 
 class MainActivity : BaseActivity<ActivityMainBinding>() {
@@ -13,7 +22,10 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
     override val viewID = R.layout.activity_main
     override val viewModel: MainViewModel by viewModels()
 
-    override fun setup() {}
+
+    override fun setup() {
+        WindowInsetsControllerCompat(window, window.decorView).isAppearanceLightStatusBars = true
+    }
 
     override fun onResume() {
         super.onResume()
@@ -27,6 +39,4 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
             binding.bubbleTabBar.setSelectedWithId(destination.id, false)
         }
     }
-
-
 }
