8cb23746d0ac2c44690b814529958ffe42381b98
Al-Taie <agprosup@gmail.com>
2021-11-18 01:20:01 +0300

    fix: implement categories back arrow

--- app/src/main/java/com/watermelon/moviesapp/ui/categories/categoriesdisplay/CategoriesDisplayFragment.kt
+++ app/src/main/java/com/watermelon/moviesapp/ui/categories/categoriesdisplay/CategoriesDisplayFragment.kt
@@ -1,14 +1,12 @@
 package com.watermelon.moviesapp.ui.categories.categoriesdisplay
 
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.watermelon.moviesapp.ui.base.BaseFragment
-import com.watermelon.moviesapp.utils.Constant
-import com.watermelon.moviesapp.utils.Event
+import com.watermelon.moviesapp.utils.EventObserver
 import watermelon.moviesapp.databinding.FragmentCategoriesDisplayBinding
 
 class CategoriesDisplayFragment : BaseFragment<FragmentCategoriesDisplayBinding>() {
@@ -17,31 +15,21 @@ class CategoriesDisplayFragment : BaseFragment<FragmentCategoriesDisplayBinding>
     override val inflate: (LayoutInflater, ViewGroup?, attachToRoot: Boolean) -> FragmentCategoriesDisplayBinding
         get() = FragmentCategoriesDisplayBinding::inflate
 
-
     private val args: CategoriesDisplayFragmentArgs by navArgs()
 
     override fun setup() {
         viewModel.getMoviesOfOneGenre(args.movieId)
         binding.categoriesRecyclerDisplay.adapter =
             CategoriesDisplayAdapter(mutableListOf(), viewModel)
-
-        viewModel.navigateToDetailsFromCategories.observe(this, ::onNavigate)
-
+        binding.goBack.setOnClickListener { findNavController().navigateUp() }
+        viewModel.navigateToDetailsFromCategories.observe(this, EventObserver { onNavigate(it) })
     }
 
-    private fun onNavigate(event: Event<Int>) {
-
-        event.getContentIfNotHandled()?.let { movieId ->
-            Log.i("movieId",movieId.toString())
-            val action =
-                CategoriesDisplayFragmentDirections.actionCategoriesDisplayFragmentToDetailsFragment(
-                    movieId
-                )
-
-
-            findNavController().navigate(action)
-        }
+    private fun onNavigate(movieID: Int) {
+        val action =
+            CategoriesDisplayFragmentDirections.actionCategoriesDisplayFragmentToDetailsFragment(
+                movieID
+            )
+        findNavController().navigate(action)
     }
-
-
 }
