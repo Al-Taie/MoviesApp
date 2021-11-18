6bbd16d4ef977c36661e8c045c906cde88193801
Al-Taie <agprosup@gmail.com>
2021-11-18 17:03:25 +0300

    fix: change base image url

--- app/src/main/java/com/watermelon/moviesapp/utils/Constant.kt
+++ app/src/main/java/com/watermelon/moviesapp/utils/Constant.kt
@@ -1,8 +1,7 @@
 package com.watermelon.moviesapp.utils
 
 object Constant {
-    const val API_KEY="bd0b11170a7c0c7abb5af89fcaf34626"
-    const val BASE_IMAGE_URL="https://www.themoviedb.org/t/p/w220_and_h330_face/"
-    const val Cast_IMAGE_URL="https://www.themoviedb.org/t/p/w138_and_h175_face/"
-    const val ID:String = "id"
+    const val API_KEY = "bd0b11170a7c0c7abb5af89fcaf34626"
+    const val BASE_IMAGE_URL = "https://image.tmdb.org/t/p/w500"
+    const val ID: String = "id"
 }
