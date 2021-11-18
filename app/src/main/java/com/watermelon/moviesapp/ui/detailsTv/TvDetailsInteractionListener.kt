1695462b79d229e53be323473aa19f40af3baac6
Sahar Qais <sahar.qais.k@gmail.com>
2021-11-19 00:38:01 +0300

    feat: navigation from similar-tv to tv details

--- app/src/main/java/com/watermelon/moviesapp/ui/detailsTv/TvDetailsInteractionListener.kt
+++ app/src/main/java/com/watermelon/moviesapp/ui/detailsTv/TvDetailsInteractionListener.kt
@@ -5,4 +5,5 @@ import com.watermelon.moviesapp.ui.base.BaseInteractionListener
 interface TvDetailsInteractionListener:BaseInteractionListener {
     fun onItemLoad(id: Int)
     fun onItemClicked(id : Int)
+    fun onSimilarItemClicked(id : Int)
 }
