4dc6679ab48736e88123cbb1023a00b048799823
Ammar Thaar <amar.at909@gmail.com>
2021-11-18 06:33:33 +0300

    fix:  encapsulate live data and change some return type

--- app/src/main/java/com/watermelon/moviesapp/ui/detailsTv/DetailsTvViewModel.kt
+++ app/src/main/java/com/watermelon/moviesapp/ui/detailsTv/DetailsTvViewModel.kt
@@ -1,4 +1,4 @@
-package com.watermelon.moviesapp.ui.detailsTv.about
+package com.watermelon.moviesapp.ui.detailsTv
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
@@ -7,18 +7,22 @@ import androidx.lifecycle.viewModelScope
 import com.watermelon.moviesapp.model.State
 import com.watermelon.moviesapp.model.repository.MovieRepository
 import com.watermelon.moviesapp.model.response.tv.TVSimilarResponse
-import com.watermelon.moviesapp.model.response.credits.Credits
 import com.watermelon.moviesapp.model.response.tv.details.TVDetailsResponse
-import com.watermelon.moviesapp.ui.home.HomeInteractionListener
+import com.watermelon.moviesapp.model.response.tvCredits.TvCredits
 import com.watermelon.moviesapp.utils.Event
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 
-class DetailsTvViewModel : ViewModel(), HomeInteractionListener {
+class DetailsTvViewModel : ViewModel(), TvDetailsInteractionListener {
 
-    var credits = MutableLiveData<State<Credits?>>()
-    var similarTv = MutableLiveData<State<TVSimilarResponse?>>()
+
+    private val _credits = MutableLiveData<State<TvCredits?>>()
+    val credits: LiveData<State<TvCredits?>> = _credits
+
+    private val _similarTv = MutableLiveData<State<TVSimilarResponse?>>()
+    val similarTv: LiveData<State<TVSimilarResponse?>> = _similarTv
+
     private val _tvDetails = MutableLiveData<State<TVDetailsResponse?>>()
     val tvDetails: LiveData<State<TVDetailsResponse?>> = _tvDetails
     private val _navigateToProfile = MutableLiveData<Event<Int>>()
@@ -28,17 +32,16 @@ class DetailsTvViewModel : ViewModel(), HomeInteractionListener {
     val navigateToItSelf: LiveData<Event<Int>> = _navigateToItSelf
 
 
-    override fun onItemClicked(id : Int) = _navigateToProfile.postValue(Event(id))
+    override fun onItemClicked(id: Int) = _navigateToProfile.postValue(Event(id))
 
     override fun onItemLoad(id: Int) {
         viewModelScope.launch {
             MovieRepository.run {
                 getTvDetails(id).collect { _tvDetails.postValue(it) }
-                getTvCredits(id).collect { credits.postValue(it) }
-                getSimilarTv(id).collect { similarTv.postValue(it) }
+                getTvCredits(id).collect { _credits.postValue(it) }
+                getSimilarTv(id).collect { _similarTv.postValue(it) }
             }
         }
     }
-
 }
 
