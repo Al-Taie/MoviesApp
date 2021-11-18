1e39a284bbac6a2c3c980c12ee0c9cf291b9f59f
Al-Taie <agprosup@gmail.com>
2021-11-18 09:32:08 +0300

    fix: optimize navbar code

--- app/src/main/java/com/watermelon/moviesapp/ui/MainActivity.kt
+++ app/src/main/java/com/watermelon/moviesapp/ui/MainActivity.kt
@@ -1,20 +1,13 @@
 package com.watermelon.moviesapp.ui
 
-import android.graphics.Color
-import android.os.Build
-import android.view.View
-import android.view.Window
+import android.os.Bundle
 import androidx.activity.viewModels
-import androidx.annotation.RequiresApi
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.navigation.findNavController
 import com.watermelon.moviesapp.ui.base.BaseActivity
 import com.watermelon.moviesapp.utils.onNavDestinationSelected
 import watermelon.moviesapp.R
 import watermelon.moviesapp.databinding.ActivityMainBinding
-import androidx.core.view.WindowInsetsControllerCompat
-
-
-
 
 
 class MainActivity : BaseActivity<ActivityMainBinding>() {
@@ -22,21 +15,28 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
     override val viewID = R.layout.activity_main
     override val viewModel: MainViewModel by viewModels()
 
-
-    override fun setup() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // MAKE STATUS BAR ICONS DARK
         WindowInsetsControllerCompat(window, window.decorView).isAppearanceLightStatusBars = true
     }
 
     override fun onResume() {
         super.onResume()
+        initNavBar()
+    }
+
+    private fun initNavBar() {
         val navController = findNavController(R.id.fragment_container_view)
 
-        binding.bubbleTabBar.addBubbleListener { id ->
-            onNavDestinationSelected(id, navController)
-        }
+        binding.bubbleTabBar.apply {
+            addBubbleListener { id ->
+                onNavDestinationSelected(id, navController)
+            }
 
-        navController.addOnDestinationChangedListener { _, destination, _ ->
-            binding.bubbleTabBar.setSelectedWithId(destination.id, false)
+            navController.addOnDestinationChangedListener { _, destination, _ ->
+                this.setSelectedWithId(destination.id, false)
+            }
         }
     }
 }
