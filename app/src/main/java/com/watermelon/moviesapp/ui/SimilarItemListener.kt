71e2b0f545537be453742f70b50ca4349b440f4c
Ammar Thaar <amar.at909@gmail.com>
2021-11-18 06:17:42 +0300

    fix:  delete unnecessary interfaces

--- app/src/main/java/com/watermelon/moviesapp/ui/SimilarItemListener.kt
+++
@@ -1,7 +0,0 @@
-package com.watermelon.moviesapp.ui
-
-import com.watermelon.moviesapp.ui.base.BaseInteractionListener
-
-interface SimilarItemListener : BaseInteractionListener {
-    fun onSimilarItemClicked(id : Int)
-}
--- app/src/main/java/com/watermelon/moviesapp/ui/detailsTv/SeasonTvInteractionListener.kt
+++
@@ -1,7 +0,0 @@
-package com.watermelon.moviesapp.ui.detailsTv
-
-import com.watermelon.moviesapp.ui.base.BaseInteractionListener
-
-interface SeasonTvInteractionListener : BaseInteractionListener {
-    fun onClickToDisplayData()
-}
